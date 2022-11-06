98c5da2794522a31859f59d8360b81d9f71430f1
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-11-06 12:40:42 +0700

    Expand the test program

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
-#include <stdio.h>
 #include "bloom.h"
 
 int main() {
     BloomFilter *f = bloom_init(10);
-    bloom_insert(f, "apple");
-    bloom_lookup(f, "apple");
+    for(int i = 0; i < 2; i++) {
+        bloom_insert(f, "apple");
+        bloom_insert(f, "coconut");
+        bloom_insert(f, "aubergine");
+        bloom_insert(f, "pomegranate");
+        bloom_insert(f, "pineapple");
+        bloom_insert(f, "grapes");
+        bloom_insert(f, "orange");
+        bloom_insert(f, "banana");
+        bloom_insert(f, "potato");
+        bloom_insert(f, "cucumber");
+    }
+    bloom_lookup(f, "tomato");
     return 0;
 }
